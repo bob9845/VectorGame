d8476f732235f8a0542c40fe05373fe88178c9da
jeron <snowtop105@gmail.com>
2016-01-27 23:31:14 -0600

    Added comments to World.java

--- src/World.java
+++ src/World.java
@@ -16,12 +16,15 @@ public class World {
 		int i, j;
 
 		m = app;
+		// Generate The World - Fill with grass
 		for(i = 0; i < 10; i++) {
 			for(j = 0; j < 8; j++) {
 				world[i][j] = GRASS;
 			}
 		}
+		// Generate A House at 2/2
 		world[2][2] = HOUSE;
+		// Load graphics for tiles
 		load_image("Grass.png", GRASS);
 		load_image("House.png", HOUSE);
 	}
@@ -38,11 +41,13 @@ public class World {
 	public void loop() {
 		int i, j;
 
+		// Draw 1st layer - the ground
 		for(i = 0; i < 10; i++) {
 			for(j = 0; j < 8; j++) {
 				m.image(img[world[i][j]], 100 * i, 100 * j);
 			}
 		}
+		// Draw 2nd layer - the structures.
 		for(i = 0; i < 10; i++) {
 			for(j = 0; j < 8; j++) {
 				if(world[i][j] == HOUSE) {
